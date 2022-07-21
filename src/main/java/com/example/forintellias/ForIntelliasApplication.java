0e16918be0ab75db9fc72e6b5dfa4234a6f3a39c
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2022-07-21 23:56:41 +0300

    Start

---
+++ src/main/java/com/example/forintellias/ForIntelliasApplication.java
@@ -0,0 +1,13 @@
+package com.example.forintellias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForIntelliasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForIntelliasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/forintellias/ForIntelliasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forintellias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForIntelliasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
